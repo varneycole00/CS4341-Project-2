f204addaa77e872dd02d366213f8797a348a9d72
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2022-02-10 15:20:21 -0500

    Mutation conflict resolution implementation for puzzle 1

--- src/NumberAllocationOperation.java
+++ src/NumberAllocationOperation.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
+
 public class NumberAllocationOperation extends DefaultOperation {
+
+    ArrayList<Float> numbers = new ArrayList<>();
+
+
     public NumberAllocationOperation(ArrayList<Float> nums, int population, int time) {
         super(population, time);
         NumberAllocation[] puzzles = new NumberAllocation[population];
-
+        this.numbers = nums;
         // create the first generation
         generateFirstGeneration(nums, puzzles);
 
@@ -118,8 +124,150 @@ public class NumberAllocationOperation extends DefaultOperation {
         Child1.setBin4(Child1Bin4);
         Child2.setBin4(Child2Bin4);
 
+        // Correct mutation conflicts here between children
+
+        correctMutationConflict(Child1, Child2);
+//        System.out.println(numberAllocationToHashMap(Child1));
+//        System.out.println(numberAllocationToHashMap(Child2));
+        hasAllNumbers(Child1);
+        hasAllNumbers(Child2);
 
         // what to return?
         return nA1;
     }
+
+    /**
+     * Takes in two children after mutation and adjusts them to be legal, valid children that will allow the algorithm
+     * to progress correctly
+     *
+     * @param child1 first offspring variation of mutation
+     * @param child2 second offspring variation of mutation
+     */
+    public void correctMutationConflict(NumberAllocation child1, NumberAllocation child2) {
+
+        HashMap<Float, Integer> child1Spread;
+        HashMap<Float, Integer> child2Spread;
+
+        boolean conflicted = true;
+
+        float swapFrom1;
+        float swapFrom2;
+
+        child1Spread = numberAllocationToHashMap(child1);
+        child2Spread = numberAllocationToHashMap(child2);
+
+        if (child1Spread.size() != 40 && child2Spread.size() != 40) { // Both children have 40 unique floats (no duplicates)
+            // Find number with occurrence == 2 or == 0 (not in hashmap) and officiate swap of 1 of each
+
+            while (conflicted) {
+                swapFrom1 = findDuplicate(child1Spread);
+                swapFrom2 = findDuplicate(child2Spread);
+
+                if (swapFrom1 != (float) 11 && swapFrom2 != (float) 11) { // Find Duplicate found a value that needs swapping in both children
+                    boolean success1 = replaceInChild(child1, swapFrom1, swapFrom2);
+                    boolean success2 = replaceInChild(child2, swapFrom2, swapFrom1);
+                    if (success1 && success2) {
+                        // Adjust the HashMaps so that future iterations do not find the same duplicates again
+                        child1Spread.put(swapFrom1, 1);
+                        child2Spread.put(swapFrom2, 1);
+                        child1Spread.put(swapFrom2, 1);
+                        child2Spread.put(swapFrom1, 1);
+
+                    } else {
+                        System.out.println("error");
+                    }
+                }
+                if (swapFrom1 == (float) 11 && swapFrom2 == (float) 11) {
+                    conflicted = false; // Will see that there is nothing else to fix and will exit the loop
+                }
+
+            }
+
+        }
+        // it can return in this case because both children have 1 occurrence of each 40 numbers
+
+
+    }
+
+    /**
+     * Creates a hashmap that makes it easier to find duplicates within a child after a crossover mutation
+     *
+     * @param child
+     * @return a Hashmap with key values of float numbers and values of occurence in child
+     */
+    public HashMap<Float, Integer> numberAllocationToHashMap(NumberAllocation child) {
+        HashMap<Float, Integer> spread = new HashMap<>();
+        for (int i = 0; i < 10; i++) { // populating hashmap
+            if (spread.containsKey(child.getBin1().get(i))) {
+                spread.put(child.getBin1().get(i), spread.get(child.getBin1().get(i)) + 1);
+            } else {
+                spread.put(child.getBin1().get(i), 1);
+            }
+            if (spread.containsKey(child.getBin2().get(i))) {
+                spread.put(child.getBin2().get(i), spread.get(child.getBin2().get(i)) + 1);
+            } else {
+                spread.put(child.getBin2().get(i), 1);
+            }
+            if (spread.containsKey(child.getBin3().get(i))) {
+                spread.put(child.getBin3().get(i), spread.get(child.getBin3().get(i)) + 1);
+            } else {
+                spread.put(child.getBin3().get(i), 1);
+            }
+            if (spread.containsKey(child.getBin4().get(i))) {
+                spread.put(child.getBin4().get(i), spread.get(child.getBin4().get(i)) + 1);
+            } else {
+                spread.put(child.getBin4().get(i), 1);
+            }
+        }
+        return spread;
+    }
+
+    public float findDuplicate(HashMap<Float, Integer> spread) {
+        for (float num : numbers) {
+            if (spread.containsKey(num)) {
+                if (spread.get(num) == 2) {
+                    return num;
+                }
+            }
+        }
+
+        return (float) 11;
+    }
+
+    public boolean replaceInChild(NumberAllocation child, float replace, float replaceWith) {
+        if (child.getBin1().contains(replace)) {
+            child.getBin1().add(child.getBin1().indexOf(replace), replaceWith);
+//            System.out.println("replaced " + replace + " with " + replaceWith + " in " + child.toString());
+            return true;
+        } else if (child.getBin2().contains(replace)) {
+            child.getBin2().add(child.getBin2().indexOf(replace), replaceWith);
+//            System.out.println("replaced " + replace + " with " + replaceWith + " in " + child.toString());
+
+            return true;
+        } else if (child.getBin3().contains(replace)) {
+            child.getBin3().add(child.getBin3().indexOf(replace), replaceWith);
+//            System.out.println("replaced " + replace + " with " + replaceWith + " in " + child.toString());
+
+            return true;
+        } else if (child.getBin4().contains(replace)) {
+            child.getBin4().add(child.getBin4().indexOf(replace), replaceWith);
+//            System.out.println("replaced " + replace + " with " + replaceWith + " in " + child.toString());
+
+            return true;
+        } else return false;
+    }
+
+    public void hasAllNumbers(NumberAllocation child) {
+        for (float num : numbers) {
+            if (!child.getBin1().contains(num) &&
+                    !child.getBin2().contains(num) &&
+                    !child.getBin3().contains(num) &&
+                    !child.getBin4().contains(num)) {
+                System.out.println(child.toString() + " doesn't contain all 40 numbers with size of: " + child.getAllNum().size());
+            }
+
+        }
+        System.out.println(child.toString() + " Contains all 40 numbers with size of: " + child.getAllNum().size());
+    }
+
 }
